75d58d3e6928f8372f09e8e937885d6a01a26486
paul-moreau <112432533+paul-moreau@users.noreply.github.com>
2023-02-03 21:21:42 +0100

    Separate data
    Separate data in 2 parts : positive ones and negatives ones

--- desktopBackground.m
+++ desktopBackground.m
@@ -18,4 +18,21 @@ Yq = 1:1/factorInterp:size(m,2);
 [XqXq,YqYq] = meshgrid(Xq,Yq);
 
 % Interpolation
-dataInterp = interpn(XX',YY',data,XqXq,YqYq,typeInterp);
+interpData = interpn(XX',YY',data,XqXq,YqYq,typeInterp);
+
+%% Separate data : positive datas and negative datas
+
+negativeData = zeros(size(interpData));
+positiveData = zeros(size(interpData));
+
+for i = 1:1:size(interpData,1)
+    for j = 1:1:size(interpData,2)
+        negativeData(i,j) = NaN;
+        positiveData(i,j) = NaN;
+        if(interpData(i,j)>=0)
+            positiveData(i,j) = interpData(i,j);
+        else
+            negativeData(i,j) = interpData(i,j);
+        end
+    end
+end
